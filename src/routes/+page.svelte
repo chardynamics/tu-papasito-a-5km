fd1bb115affe3dba1f7baba81183e80278e2b0a7
Charles Duong <meridiancha.rdynamics@gmail.com>
2026-08-14 17:26:07 -0500

    More changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Buttons from '../lib/components/buttons.svelte'
+    import NameDescription from '../lib/components/name-description.svelte'
+</script>
+
+<div class="flex justify-center items-center min-h-screen">
+    <div class="child">
+        <Buttons />
+        <NameDescription />
+    </div>
+</div>
