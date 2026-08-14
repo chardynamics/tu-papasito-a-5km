42c6802ab696e999d17bce50f5c5c79c01bf3b45
Charles Duong <meridiancha.rdynamics@gmail.com>
2026-08-14 18:27:25 -0500

    text done??

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script>
     import Buttons from '../lib/components/buttons.svelte'
     import NameDescription from '../lib/components/name-description.svelte'
+    import { globalState } from '$lib/state.svelte'
 </script>
 
 <div class="flex justify-center items-center min-h-screen">
     <div class="child">
+        <NameDescription name={globalState.character.name} age={globalState.character.age} height={globalState.character.height} description={globalState.character.description}/>
         <Buttons />
-        <NameDescription />
     </div>
 </div>
