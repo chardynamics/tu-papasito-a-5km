393492ce5d1524bc69ff9313951fca0566dbe4c1
Charles Duong <meridiancha.rdynamics@gmail.com>
2026-08-14 18:41:01 -0500

    changing directory

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,15 @@
     import { globalState } from '$lib/state.svelte'
 </script>
 
-<div class="flex justify-center items-center min-h-screen">
-    <div class="child">
-        <NameDescription name={globalState.character.name} age={globalState.character.age} height={globalState.character.height} description={globalState.character.description}/>
-        <Buttons />
+<div class="grid w-full">
+    <div class="col-start-1 row-start-1 flex justify-center items-center">
+        <img src="/assets/bg.png" alt="background">
+    </div>
+
+    <div class="col-start-1 row-start-1 flex justify-center items-center min-h-screen z-10">
+        <div class="child">
+            <NameDescription name={globalState.character.name} age={globalState.character.age} height={globalState.character.height} description={globalState.character.description}/>
+            <Buttons />
+        </div>
     </div>
 </div>
