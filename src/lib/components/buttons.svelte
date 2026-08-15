ffad0cf6c7006ddefd1e95b6f7bd76d7d16190fd
Charles Duong <meridiancha.rdynamics@gmail.com>
2026-08-15 00:04:48 -0500

    Update buttons.svelte

--- src/lib/components/buttons.svelte
+++ src/lib/components/buttons.svelte
@@ -20,23 +20,16 @@ function changeCharacter() {
 		onclick={() => {
             changeCharacter()
         }}
-		class="rounded-full border-3 size-25 border-red-500 hover:cursor-pointer flex justify-center items-center"
+		class="rounded-full size-25 hover:cursor-pointer flex justify-center items-center"
 	>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="60"
-			height="60"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			class="lucide lucide-x-icon lucide-x stroke-red-500"
-		>
-			<path d="M18 6 6 18" />
-			<path d="m6 6 12 12" />
-		</svg>
+
+        <img
+            src="/assets/nope.png"
+            alt="favicon"
+            width="100"
+            height="100"
+            style="display:block; width:100px; height:100px; image-rendering: pixelated; image-rendering: crisp-edges;"
+        />
 	</button>
 
 	<button
@@ -45,23 +38,14 @@ function changeCharacter() {
 		onclick={() => {
             changeCharacter()
         }}
-		class="rounded-full border-3 size-25 border-green-500 hover:cursor-pointer flex justify-center items-center"
+		class="rounded-full size-25 hover:cursor-pointer flex justify-center items-center"
 	>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="60"
-			height="60"
-			viewBox="0 0 24 24"
-			fill="currentColor"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			class="lucide lucide-heart-icon lucide-heart stroke-green-500 fill-green-500"
-		>
-			<path
-				d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"
-			/>
-		</svg>
+        <img
+            src="/assets/caliente.png"
+            alt="favicon"
+            width="100"
+            height="100"
+            style="display:block; width:100px; height:100px; image-rendering: pixelated; image-rendering: crisp-edges;"
+        />
 	</button>
 </div>
